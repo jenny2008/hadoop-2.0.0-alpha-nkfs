25e922937afe9e85ff6352ed6339b2d4ec44904b
jenny2008.1 <jenny2008.1@gmail.com>
2012-08-15 17:18:32 +0800

    create distributed environment

--- dist/etc/hadoop/hadoop-env.sh
+++ dist/etc/hadoop/hadoop-env.sh
@@ -1,10 +1,10 @@
-JAVA_HOME=/opt/sun-jdk-1.6.0.31
+JAVA_HOME=/home/fengqingqing/exp/java/jdk1.7.0_03
 this="${BASH_SOURCE-$0}"
 conf_dir=$(cd -P -- "$(dirname -- "$this")" && pwd -P)
 hadoop_base="${conf_dir}/../../"
 hadoop_base=$(cd -P -- "${hadoop_base}" && pwd -P)
-HADOOP_CLASSPATH="${hadoop_base}/../classes"
-HADOOP_CLASSPATH=$(cd -P -- "${HADOOP_CLASSPATH}" && pwd -P)
+#HADOOP_CLASSPATH="${hadoop_base}/../classes"
+#HADOOP_CLASSPATH=$(cd -P -- "${HADOOP_CLASSPATH}" && pwd -P)
 HADOOP_HOME=$hadoop_base
 YARN_HOME=$HADOOP_HOME
 HADOOP_COMMON_HOME=$HADOOP_HOME
@@ -14,10 +14,9 @@ export HADOOP_HOME
 export HADOOP_COMMON_HOME
 export HADOOP_HDFS_HOME
 
+HADOOP_CLASSPATH=${hadoop_base}/magicube/magicube.jar
+export HADOOP_CLASSPATH
 
-# echo "HADOOP_COMMON_HOME = $HADOOP_COMMON_HOME"
-# echo "HADOOP_HOME = $HADOOP_HOME"
-# echo "YARN_HOME = $YARN_HOME"
-# #echo "HADOOP_CLASSPATH=$HADOOP_CLASSPATH"
-
+YARN_APPLICATION_CLASSPATH=${HADOOP_CLASSPATH}
+export YARN_APPLICATION_CLASSPATH
 
